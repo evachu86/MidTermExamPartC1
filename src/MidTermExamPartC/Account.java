651f0d82e5172c0a8b0855719ed85b536b5a2650
evachu86 <evachu86@users.noreply.github.com>
2021-07-09 00:29:25 +0800

    Final version.

--- src/MidTermExamPartC/Account.java
+++ src/MidTermExamPartC/Account.java
@@ -1,93 +1,131 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Name: Yi-Wen Chu    991624614
+ * Assignment: Midterm 
+ * Program: Computer Systems Technology -
+ * 	Software Development and Network Engineering
+ * File: Account.java
+ * Other Files in this Project: 
+ * 
+ * Main class: AccountTester.java
+ * 
+ * Date: Jul 8, 2021
+ * 
+ * Description: Model for storing account information.
  */
+
 package MidTermExamPartC;
 
 /**
+ * The Class Account.
  *
- * @author 
+ * @author
  */
 public class Account {
-    
- 
-    private String userName;//the userID
-    private String accountNum; // account number
-//    private String accountType;// the user's account type
-    private AccountType accountType;// the user's account type
-//    public final static String [] account_types={"Checking","CC","StudentLoan"};
-    public enum AccountType {Checking, Cc, StudentLoan, YiwenStudentAccount};
 
-    /**
-     * A constructor that takes in the userName and the Account type
-     *
-     * @param chosenName the ID to assign to this user
-     * @param chosenType the users account type
-     */
-    public Account(String chosenName,AccountType chosenType) {
-        userName = chosenName;
-        accountType = chosenType;
-        accountNum= "A-"+(int)(1000+Math.random()*9000);
-    }
+	private String userName;// the userID
+	private String accountNum; // account number
+	private AccountType accountType;// the user's account type
 
-    /**
-     * A getter for the userName
-     *
-     * @return the userName
-     */
-    public String getUserName() {
-        return userName;
-    }
-    
-     public String getAccountNum() {
-        return accountNum;
-    }
+	/**
+	 * The Enum AccountType.
+	 *
+	 * @author Yi-Wen Chu Computer Systems Technology Software Development and
+	 *         Network Engineering
+	 */
+	public enum AccountType {
 
+		/** The Checking. */
+		Checking("Checking"),
+		/** The Cc. */
+		Cc("CC"),
+		/** The Student loan. */
+		StudentLoan("Student Loan"),
+		/** The Yiwen student account. */
+		YiwenStudentAccount("Yiwen Studnet Account");
 
-    /**
-     * @param userName the userName to set
-     */
-    public void setUserID(String name) {
-        userName = name;
-    }
+		private String showName;
+		
+		AccountType (String showName) {
+			this.showName = showName;
+		}
+		
+		public String getName() {
+			return showName;
+		}
+	};
 
-    /**
-     * A getter for the Account Type
-     *
-     * @return the type
-     */
+	/**
+	 * A constructor that takes in the userName and the Account type.
+	 *
+	 * @param chosenName the ID to assign to this user
+	 * @param chosenType the users account type
+	 */
+	public Account(String chosenName, AccountType chosenType) {
+		userName = chosenName;
+		accountType = chosenType;
+		accountNum = "A-" + (int) (1000 + Math.random() * 9000);
+	}
+
+	/**
+	 * A getter for the userName.
+	 *
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * Gets the account num.
+	 *
+	 * @return the account num
+	 */
+	public String getAccountNum() {
+		return accountNum;
+	}
+
+	/**
+	 * Sets the user ID.
+	 *
+	 * @param name the new user ID
+	 */
+	public void setUserID(String name) {
+		userName = name;
+	}
+
+	/**
+	 * A getter for the Account Type.
+	 *
+	 * @return the type
+	 */
 	public AccountType getAccountType() {
 		return accountType;
 	}
-//    public String getAccountType() {
-//        return this.AccountType;
-//    }
-	
 
-    /**
-     * @param genre the genre to set
-     */
+	/**
+	 * Sets the account type.
+	 *
+	 * @param accountType the new account type
+	 */
 	public void setAccountType(AccountType accountType) {
 		this.accountType = accountType;
 	}
-//    public void setAccountType(String type) {
-//        this.AccountType = type;
-//    }
 
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	@Override
 	public String toString() {
-		
-		String pattern = "%-12s  %-15s  %-20s";
-		return String.format(pattern, 
-				"Account No.", "User name", "Account Type") 
+
+		String pattern = "%-12s  %-15s  %-25s";
+		return String.format(pattern, "Account No.", "User name", "Account Type")
 				+ "\n=================================================\n"
 				+ String.format(pattern, 
-						accountNum, userName, accountType.name());
+						accountNum, userName, accountType.getName());
 	}
-	
-	
 
 //end class
-   
+
 }
--- src/MidTermExamPartC/AccountTester.java
+++ src/MidTermExamPartC/AccountTester.java
@@ -1,40 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor. 
+/* 
+ * Name: Yi-Wen Chu    991624614
+ * Assignment: Midterm 
+ * Program: Computer Systems Technology -
+ * 	Software Development and Network Engineering
+ * File: AccountTester.java
+ * Other Files in this Project: 
+ * AccountTester.java
+ * Main class: AccountTester.java
+ * 
+ * Date: Jul 9, 2021
+ * 
+ * Description: Test class for AccountTesting
  */
 package MidTermExamPartC;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import MidTermExamPartC.Account.AccountType;
 
 /**
+ * The Class AccountTester.
  *
  * @author 
  */
 public class AccountTester {
+    
+    /** The input. */
     public static Scanner input;
     
     /* Add your code here */
 	
-	public static void main(String[] args) {
+	/**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
 		
 		input = new Scanner(System.in);
 		
+		// Step 1: test function of Account Creation.
 		createAccount(input);
 	}
 	
+	/**
+	 * Creates the account.
+	 *
+	 * @param input the input
+	 */
 	public static void createAccount(Scanner input) {
 		
 		System.out.println("Welcome to account creation function!");
 		
+		// Step 2: The user is asked to input the user's name.
 		String userName = InqUserName(input);
 		System.out.println();
+		
+		// Step 3: The user is asked to choose their account type 
+		// 			from the account type list.
 		AccountType accountType = InqAccountType(input);
 		System.out.println();
 		
 		Account newAccount = new Account(userName, accountType);
 		
+		// Step 4: A confirmation message 
+		// 		including account information is shown to the user.
 		System.out.println();
 		System.out.println("Your account:");
 		System.out.println(newAccount);
@@ -55,17 +85,35 @@ public class AccountTester {
 				+ "according to the following list: "
 				+ "(Please enter completely same as shown)");
 		for(AccountType type: AccountType.values()) {
-			System.out.println(type);
+			System.out.println((type.ordinal()+1) +". " 
+									+ type.getName());
 		}
 		System.out.print("Your account type:");
 		
 	
 		try {
-			accountType = AccountType.valueOf(input.next());
-		} catch(IllegalArgumentException e) {
-			System.out.println(
-					"The account type input not in the list, "
-					+ "please enter again.");
+			int inputNo = input.nextInt();
+			
+			switch(inputNo) {
+				case 1: accountType = AccountType.Checking;
+					break;
+				case 2: accountType = AccountType.Cc;
+					break;
+				case 3: accountType = AccountType.StudentLoan;
+					break;
+				case 4: accountType = AccountType.YiwenStudentAccount;
+					break;
+				default: 
+					throw new InputMismatchException
+							("The input number not in the list, "
+							+ "please enter again.\n");
+			}
+			
+		} catch(InputMismatchException e) {
+			String errMsg = (e.getMessage() == null)?
+					"Please input the number of the account type.\n":
+						e.getMessage();
+			System.out.println(errMsg);
 			input.nextLine();
 			InqAccountType(input);
 		}
